0a18483562ba2bf77544c71273c8f6e3150fb1ae
Keith Cirkel <keithamus@users.noreply.github.com>
2017-04-10 22:15:26 +0100

    style: fix lint errors in new config

--- test/node.js
+++ test/node.js
@@ -1,4 +1,5 @@
 'use strict';
+
 var assert = require('simple-assert');
 var type = require('..');
 var isNode = typeof process !== 'undefined' && typeof process.release === 'object' && process.release.name;
