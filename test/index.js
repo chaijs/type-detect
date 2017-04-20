aa0ea9c2495e62018c5b1a7602e9bd7e7dd8273c
Lucas Fernandes da Costa <fernandesdacostalucas@gmail.com>
2017-04-19 21:30:14 -0300

    Revert "fix: favour extended array toStringTag where available"

--- test/index.js
+++ test/index.js
@@ -259,6 +259,7 @@ describe('Generic', function () {
       Object.prototype.toString = originalObjectToString; // eslint-disable-line no-extend-native
     });
 
+
     it('plain object', function () {
       var obj = {};
       obj[Symbol.toStringTag] = function () {
