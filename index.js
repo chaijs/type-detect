5336711137c9714a20e6ef6833b9316411429e1e
Keith Cirkel <keithamus@users.noreply.github.com>
2017-09-06 21:35:56 +0100

    fix up eslint workarounds

--- index.js
+++ index.js
@@ -7,15 +7,13 @@
  */
 var promiseExists = typeof Promise === 'function';
 
-/* eslint-disable */
 // See http://stackoverflow.com/a/6930376
-var globalObject;
+var globalObject = null;
 try {
-  globalObject = Function('return this')() || (42, eval)('this');
-} catch (e) {
-  globalObject = window;
+  globalObject = Function('return this')() || (0, eval)('this'); // eslint-disable-line no-new-func, no-eval
+} catch (ignoredError) {
+  globalObject = window; // eslint-disable-line no-undef
 }
-/* eslint-enable */
 
 /*
  * All of these attributes must be available on the global object for the current environment
