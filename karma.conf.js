6536e60e6da22b371ef7d6ffe242e6449c5ba0df
Keith Cirkel <keithamus@github.com>
2017-11-02 10:45:09 +0000

    test(karma): reconfigure karma to run in 4 descrete modes
    This reconfigures Karma for various CI tasks to run in 4 descrete modes.
    
     - Run locally, this will detect which browsers are available and run them.
     - Run in AppVeyor, this will run `Firefox`, `ChromeHeadless` and `IE`(11) (which are available on all AppVeyor VMs)
     - Run in Travis, this will run `FirefoxHeadless` and `ChromeHeadless` (which are both available in Travis with some config)
     - Run with saucelabs environment vars, it will run Safari, Edge and IE10

--- karma.conf.js
+++ karma.conf.js
@@ -1,85 +1,81 @@
-/* eslint-disable no-process-env */
 const packageJson = require('./package.json');
-const defaultTimeout = 120000;
+let browsers = [ 'ChromeHeadless' ];
+let build = 'localbuild';
+let branch = 'local';
+let job = Date.now();
+let startConnect = false;
+const reporters = [ 'progress', 'coverage' ];
+const tags = [
+  `${ packageJson.name }@${ packageJson.version }`,
+];
+const frameworks = [ 'mocha' ];
+
+/* eslint-disable no-process-env */
+const debug = Boolean(process.env.npm_config_debug);
+if (process.env.SAUCE_ACCESS_KEY && process.env.SAUCE_USERNAME) {
+  browsers = [ 'SauceEdgeLatest', 'SauceInternetExplorerTen', 'SauceSafariLatest' ];
+  tags.push(`${ process.env.SAUCE_USERNAME }@${ branch }`);
+  reporters.push('saucelabs');
+} else if (process.env.APPVEYOR) {
+  browsers = [ 'Firefox', 'ChromeHeadless', 'IE' ];
+  branch = process.env.APPVEYOR_REPO_BRANCH;
+  job = process.env.APPVEYOR_JOB_NUMBER;
+  build = `appveyor@${ process.env.APPVEYOR_JOB_NUMBER }`;
+} else if (process.env.TRAVIS) {
+  browsers = [ 'FirefoxHeadless', 'ChromeHeadless' ];
+  branch = process.env.TRAVIS_BRANCH;
+  job = process.env.TRAVIS_JOB_NUMBER;
+  build = `travis@${ process.env.TRAVIS_JOB_NUMBER }`;
+  startConnect = true;
+} else {
+  browsers = [];
+  frameworks.push('detectBrowsers');
+}
+/* eslint-enable */
+
+tags.push(build);
 module.exports = function configureKarma(config) {
-  const localBrowsers = [
-    'PhantomJS',
-  ];
-  const sauceLabsBrowsers = {
-    SauceChromeLatest: {
-      base: 'SauceLabs',
-      browserName: 'Chrome',
-    },
-    SauceFirefoxLatest: {
-      base: 'SauceLabs',
-      browserName: 'Firefox',
-    },
-    SauceSafariLatest: {
-      base: 'SauceLabs',
-      browserName: 'Safari',
-      platform: 'OS X 10.11',
-    },
-    SauceInternetExplorerLatest: {
-      base: 'SauceLabs',
-      browserName: 'Internet Explorer',
-    },
-    SauceInternetExplorerOldestSupported: {
-      base: 'SauceLabs',
-      browserName: 'Internet Explorer',
-      version: 9,
-    },
-    SauceEdgeLatest: {
-      base: 'SauceLabs',
-      browserName: 'MicrosoftEdge',
-    },
-    SauceAndroidLatest: {
-      base: 'SauceLabs',
-      browserName: 'Android',
-    },
-  };
   config.set({
     basePath: '',
-    browsers: localBrowsers,
-    logLevel: process.env.npm_config_debug ? config.LOG_DEBUG : config.LOG_INFO,
-    frameworks: [ 'mocha' ],
-    files: [ 'type-detect.test.js' ],
-    exclude: [],
-    reporters: [ 'progress', 'coverage' ],
+    browsers,
+    singleRun: true,
+    logLevel: debug ? config.LOG_DEBUG : config.LOG_INFO,
+    frameworks,
+    files: [ `${ packageJson.name }.test.js` ],
+    reporters,
     coverageReporter: {
       type: 'lcov',
       dir: 'coverage',
     },
-    port: 9876,
-    colors: true,
-    concurrency: 3,
-    autoWatch: false,
-    captureTimeout: defaultTimeout,
-    browserDisconnectTimeout: defaultTimeout,
-    browserNoActivityTimeout: defaultTimeout,
-    singleRun: true,
-  });
-
-  if (process.env.SAUCE_ACCESS_KEY && process.env.SAUCE_USERNAME) {
-    const branch = process.env.TRAVIS_BRANCH || 'local';
-    let build = 'localbuild';
-    if (process.env.TRAVIS_JOB_NUMBER) {
-      build = `travis@${ process.env.TRAVIS_JOB_NUMBER }`;
-    }
-    config.reporters.push('saucelabs');
-    config.set({
-      customLaunchers: sauceLabsBrowsers,
-      browsers: localBrowsers.concat(Object.keys(sauceLabsBrowsers)),
-      sauceLabs: {
-        testName: packageJson.name,
-        tunnelIdentifier: process.env.TRAVIS_JOB_NUMBER || new Date().getTime(),
-        recordVideo: true,
-        startConnect: ('TRAVIS' in process.env) === false,
-        tags: [
-          `typeDetect_${ packageJson.version }`,
-          `${ process.env.SAUCE_USERNAME }@${ branch }`,
-          build,
-        ],
+    customLaunchers: {
+      FirefoxHeadless: {
+        base: 'Firefox',
+        flags: [ '-headless' ],
       },
-    });
-  }
+      SauceSafariLatest: {
+        base: 'SauceLabs',
+        browserName: 'Safari',
+        platform: 'OS X 10.11',
+      },
+      SauceInternetExplorerTen: {
+        base: 'SauceLabs',
+        browserName: 'Internet Explorer',
+        version: 10,
+      },
+      SauceEdgeLatest: {
+        base: 'SauceLabs',
+        browserName: 'MicrosoftEdge',
+      },
+    },
+    detectBrowsers: {
+      enabled: true,
+      usePhantomJS: false,
+    },
+    sauceLabs: {
+      testName: packageJson.name,
+      tunnelIdentifier: job,
+      startConnect,
+      tags,
+    },
+  });
 };
